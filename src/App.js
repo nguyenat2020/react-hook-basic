fc97e789f2904bbab4bafddd835f2049c608fe8a
nguyenat2020 <great.expectations7760@gmail.com>
2023-08-24 13:28:32 +0900

    component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav.js';
 
 function App() {
+  let obj = {name : 'Ares7760', year: 2023};
+  let link = 'https://www.youtube.com/watch?v=Y9gTouaZJ5s&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=10';
+
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and NguyenAT</h1>
+        <h1>Hello world with React and {obj.name} in {obj.year}!</h1>
+        <p>{JSON.stringify(obj)}</p>
+        <a href={link} target="_blank">Visit Hoi Dan IT channel</a>
       </header>
     </div>
   );
